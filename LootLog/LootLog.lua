0451d1bc840c5aeb2d6c22a4d9ded54060f50f94
Donovan Levinson <skipatrol@gmail.com>
2023-03-17 16:56:25 -1000

    Updated addon to include whether item contains socket or tert procs.

--- LootLog/LootLog.lua
+++ LootLog/LootLog.lua
@@ -10,11 +10,32 @@ local function eventHandler(self, event, ...)
 
         if itemLink then
             local itemName, itemLink, itemRarity = GetItemInfo(itemLink)
+            local itemStats = GetItemStats(itemLink)           
+
+            local hasSocket = "No"
+            if itemStats["EMPTY_SOCKET_PRISMATIC"] then
+                hasSocket = "Yes"
+            end
+
+            local tertiaryStat = "None"
+            if itemStats["ITEM_MOD_CR_SPEED_SHORT"] then
+                tertiaryStat = "Speed"
+            elseif itemStats["ITEM_MOD_CR_LIFESTEAL_SHORT"] then
+                tertiaryStat = "Leech"
+            elseif itemStats["ITEM_MOD_CR_AVOIDANCE_SHORT"] then
+                tertiaryStat = "Avoidance"
+            elseif itemStats["ITEM_MOD_CR_STURDINESS_SHORT"] then
+                tertiaryStat = "Indestructible"
+            end
+
+            print("Looted "..itemName.." (Socket: "..hasSocket.." / Tert: "..tertiaryStat..")")
 
             lootTable[#lootTable + 1] = {
                 name = itemName,
                 rarity = itemRarity,
                 link = itemLink,
+                socket = hasSocket,
+                tert = tertiaryStat,
                 timestamp = time(),
             }
         end
